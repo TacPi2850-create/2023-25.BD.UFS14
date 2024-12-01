76bd165c3a33e76137bce4dccff5e28c02a3d893
TacPi2850-create <tacunan1995@gmail.com>
2024-12-01 22:40:47 +0000

    holas

--- MyProjFolder/function_app.py
+++ MyProjFolder/function_app.py
@@ -1,27 +1,36 @@
 import azure.functions as func
-import datetime
-import json
+import requests
+import random
 import logging
 
 app = func.FunctionApp()
 
-@app.route(route="MyHttpTrigger", auth_level=func.AuthLevel.ANONYMOUS)
-def MyHttpTrigger(req: func.HttpRequest) -> func.HttpResponse:
+def get_question():
+    """Get a single random trivia question."""
+    url = "https://opentdb.com/api.php?amount=1"
+    response = requests.get(url)
+    data = response.json()['results'][0]
+   
+    question = data['question']
+    correct = data['correct_answer']
+    incorrect = data['incorrect_answers']
+   
+    # Mix up the answers
+    all_answers = [correct] + incorrect
+    random.shuffle(all_answers)
+   
+    return question, all_answers, correct
+
+@app.route(route="TriviaHttpTrigger", auth_level=func.AuthLevel.ANONYMOUS)
+def TriviaHttpTrigger(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+    # Get a question
+    question, answers, correct = get_question()
+   
+    # Format the response as a string
+    response = f"Question: {question}\n\n"
+    for i, answer in enumerate(answers, 1):
+        response += f"{i}. {answer}\n"
+   
+    return func.HttpResponse(response)
--- test_snapshot.py
+++
@@ -1,7 +0,0 @@
-def coso():    
-    return'''due righe
-          seconda riga'''
-            
-def test_function_output_with_snapshot(snapshot):
-    snapshot.snapshot_dir = 'snapshots'  # This line is optional.
-    snapshot.assert_match(coso(), 'coso_output.txt')
